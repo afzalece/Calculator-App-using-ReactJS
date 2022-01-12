92b6bd8231b432651406ffe6af38131d767a7096
Afzal Hossain <90591676+afzalece@users.noreply.github.com>
2022-01-12 11:51:35 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sum,  {sub, multi, div} from './Calc'
+import App from './App'
 
 ReactDOM.render(
-  <> 
-  <h1>Calculator App using React</h1>
-  <ul>
-    <li>Addition of two number is {sum()}</li>
-    <li>Subtruction of two number is  {sub()}</li>
-    <li>Division of two number is {div()}</li>
-    <li>Multiplication of two number is  {multi()}</li>
-   
-  </ul>
-  </>
+  <App/>
 , document.getElementById('root')
   );
